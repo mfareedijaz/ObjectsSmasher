5a04c264fa3fc696b0d94b0565279da297ac9703
fareed <fareedijaz40@gmail.com>
2024-08-12 15:38:52 +0500

    First version of ObjectsSmasher

--- webpack.config.js
+++ webpack.config.js
@@ -1,12 +1,13 @@
-const path = require('path');
+// webpack.config.js
+const path = require("path");
 
 module.exports = {
-    entry: './main.js',
-    output: {
-        filename: 'objectssmasher.min.js',
-        path: path.resolve(__dirname, 'dist'),
-        library: 'ObjectsSmasher',
-        libraryTarget: 'umd',
-    },
-    mode: 'production',
+  entry: "./main.js",
+  output: {
+    filename: "objectssmasher.min.js",
+    path: path.resolve(__dirname, "dist"),
+    library: "ObjectsSmasher",
+    libraryTarget: "umd",
+  },
+  mode: "production",
 };
